c709e1d2ee05d9e8006d32fc016a4ccd6597572e
Chris Breaux and Ward Chamberlain <chris+ward@>
2019-01-29 11:53:23 -0800

    add email forms and store them as objects in Contact object

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
-function Contact(firstName, lastName, phoneNumber) {
-  this.firstName = firstName,
-  this.lastName = lastName,
-  this.phoneNumber = phoneNumber,
-  this.fullName = function() {
-    return this.firstName + " " + this.lastName;
-  }
+function Email(address) {
+  this.address = address;
+  // this.type = type;
 }
 
 
 
 
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+  this.emails = [];
+  this.fullName = function() {
+    return this.firstName + " " + this.lastName;
+  }
+}
+
+Contact.prototype.addEmails = function(emailInputs) {
+  var emails = [];
+
+  for(var i = 0; i < emailInputs.length; i++) {
+    this.emails.push(emailInputs[i]);
+  }
+  // emailInputs.forEach(function(email) {
+  //   emails.push(email.address);
+  // }); The reason is that when passing a regular function as a callback, when it is invoked the this is not actually preserved.
+
+
+}
 
 function AddressBook() {
   this.uniqueId = 0,
@@ -69,10 +87,20 @@ function displayContactDetails(addressBookToDisplay) {
 
 function showContact(contactId) {
   var contact = addressBook.findContact(contactId);
+  var emails = "";
   $("#show-contact").show();
   $(".first-name").html(contact.firstName);
   $(".last-name").html(contact.lastName);
   $(".phone-number").html(contact.phoneNumber);
+  //$(".email").html(contact.emails[0].address);
+  contact.emails.forEach(function(email){
+    emails +=("<li>" + email.address + "</li>");
+    console.log(emails);
+  });
+  $(".emails").html(emails);
+
+
+
   var buttons = $("#buttons");
   buttons.empty();
   buttons.append("<button class='deleteButton' id=" +  + contact.uniqueId + ">Delete</button>");
@@ -96,13 +124,28 @@ $(document).ready(function() {
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
     var inputtedPhoneNumber = $("input#new-phone-number").val();
+    var inputtedEmails = [];
+
+    $("input.email").each(function(){
+      if($(this).val()) {
+        var inputtedEmail = new Email($(this).val());
+        inputtedEmails.push(inputtedEmail);
+      }
+    });
+
+    console.log(inputtedEmails);
+
+    var inputtedEmail = $("input#email").val();
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-phone-number").val("");
+    $("input#email").val("");
 
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    newContact.addEmails(inputtedEmails);
+    console.log(newContact);
     addressBook.addContact(newContact);
     displayContactDetails(addressBook);
   })
